268ffb534dbf7f1babe2002b1aa91be0a1bfbd55
nanggi4 <nkhun1006@gmail.com>
2021-08-06 23:55:25 +0900

    20210806 update

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,20 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import clsx from 'clsx';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import { withStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
+import { useTheme, withStyles } from '@material-ui/core/styles';
+import { Container, Button } from '@material-ui/core';
 
 const Counter = (props) => {
   const classes = props.classes;
+  const [count, setCount] = useState(0);
 
   return (
     <React.Fragment>
       <CssBaseline />
       <Container maxWidth="sm" className={clsx(classes.mainContainer)}>
-        <Typography variant="h3" gutterBottom>
-          React Counter
-        </Typography>        
+        <h1>{count}</h1>
+        <Button variant="contained" size="large" color="primary" className={classes.margin}>
+          +
+        </Button>
+        <Button variant="contained" size="large" color="primary" className={classes.margin}>
+          -
+        </Button>                
       </Container>
     </React.Fragment>
   );
